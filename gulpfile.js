53149f804acab977066d2fee87a7a399d28fedde
Len Serrano <len.serrano.g@gmail.com>
2020-06-15 17:07:33 -0500

    se cambian las funciones de gulp para detectar cambios en todos los archivos scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,29 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload;
-var sass        = require('gulp-sass');
- 
-// Tareas para BrowserSync
-gulp.task('browser-sync', function() {
-    //Archivos a observar
-    var files = [
-        './assets/sass/*.scss',
-        './style.css', //Nombre de salida del compilado de Sass
-        './assets/js/*.js',
-    ];
- 
-    //Se inicializa BrowserSync
-    browserSync.init(files, {
-        proxy: "localhost:8888/cltvo-sass-for-html", //Servidor y nombre del proyecto
-        notify: false
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//Compilar SASS a CSS
+
+function stylesheet(){
+    //Ruta de los archivos SCSS
+    return gulp.src('./assets/scss/**/*.scss')
+               .pipe(sass().on('error', sass.logError))
+               .pipe(gulp.dest('./assets/css'))
+               .pipe(browserSync.stream());
+}
+
+//
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir : './'
+        }
     });
-});
- 
-// Tarea para Sass, observa los cambios
-gulp.task('sass', function () {
-    return gulp.src('./assets/sass/style.scss') //Archivo scss contenedor -> da de salida style.css
-        .pipe(sass())
-        .pipe(gulp.dest('./')) //Carpeta destino del compilado
-        .pipe(reload({stream:true}));
-});
+    gulp.watch('./assets/scss/**/*.scss', stylesheet);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload);
+}
 
-//Necesario para v4 gulp.series() para encadenar la tarea
-gulp.task('watch-sass', function () {
-    gulp.watch('./assets/sass/style.scss', gulp.series('sass'));
-});
-
-
-
-// Tarea default al correr comando Gulp, se ejecutan simultáneamente las tareas
-gulp.task('default', gulp.parallel('watch-sass', 'browser-sync'));
+exports.stylesheet = stylesheet;
+exports.watch = watch;
